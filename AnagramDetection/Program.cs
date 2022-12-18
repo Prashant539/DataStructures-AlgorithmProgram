63e5ec26cd5d80e58bfbe8fd26abda4c22ec1849
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-18 23:26:00 +0530

    [Prashant]:An Anagram Detection Example

---
+++ AnagramDetection/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace AnagramDetection
+{
+    internal class Program
+    {
+       static void Main(string[] args)
+        {
+            Anagram anagram = new Anagram();
+            anagram.AnagramPrint();
+
+        }
+    }
+}
