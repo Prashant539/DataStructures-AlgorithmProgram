2200dc2b2d862c969ca25f6d3ffbb89a07f19ae3
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-18 23:19:56 +0530

    [Prashant]:Find the Prime Number

---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace PrimeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isPrime = true;
+            Console.WriteLine("Prime Numbers : ");
+            for (int i = 2; i <= 1000; i++)
+            {
+                for (int j = 2; j <= 1000; j++)
+                {
+
+                    if (i != j && i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+
+                }
+                if (isPrime)
+                {
+                    Console.Write("\t" + i);
+                }
+                isPrime = true;
+            }
+            Console.ReadKey();
+        }
+    }
+}
+        
+    
+
